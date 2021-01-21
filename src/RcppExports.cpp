55422c68e14e386910a7d0fa568bda73f490f571
Rich FitzJohn <rich.fitzjohn@gmail.com>
2021-01-21 22:36:28 +0000

    Redocument
    One holdover from package rename here

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -20,7 +20,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_reacttemporal2(DllInfo *dll) {
+RcppExport void R_init_reactidd(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
