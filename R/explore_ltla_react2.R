5733c597d301bb00a6d39dcc7123d404eae65b62
Steven Riley <sr@stevenriley.net>
2021-07-01 10:22:48 +0100

    Added a little to explore_ltla_react2.R

--- R/explore_ltla_react2.R
+++ R/explore_ltla_react2.R
@@ -11,8 +11,19 @@ if (false) {
     hist(df_ab_prev$Overall.n..antibody.positive,breaks = seq(0,2000,10))
     hist(df_ab_prev$Overall.weighted.and.adjusted.antibody.prevalence,
          breaks = seq(0,100,5),
-         main = "High variance of prevalence of antibody poititivy by LTLA",
+         main = "High variance of prevalence of antibody positivity by LTLA",
          xlab = "Weighted adjusted prevalence",
          ylab = "Frequency of LTLAs")
-        
+
+    ## Load up LTLA case data from UK gov dashboard
+    ## install_github("c97sr/idd")
+    ## library(idd)
+    x <- idd::load_uk_cov_data(at="ltla")
+
+    head(x)
+    head(df_ab_prev)
+
+    ## need to merge on areaCode and LTLA.name once have made a
+    ## growth rate calculation for the dashboard data
+    
 }
