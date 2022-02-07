da528b8373cc9f3146892662ea48cb950318b78f
Eales96 <odeales@gmail.com>
2022-02-07 12:48:25 +0000

    Can now plot incidence from model fit

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -15,6 +15,7 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 RcppExport SEXP _rcpp_module_boot_stan_fit4b_splines_actual_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4b_splines_actual_phe_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4b_splines_actual_weighted_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4b_splines_actual_weighted_incidence_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4linear_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4linear_phe_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4linear_weighted_mod();
@@ -25,6 +26,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_stan_fit4b_splines_actual_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4b_splines_actual_mod, 0},
     {"_rcpp_module_boot_stan_fit4b_splines_actual_phe_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4b_splines_actual_phe_mod, 0},
     {"_rcpp_module_boot_stan_fit4b_splines_actual_weighted_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4b_splines_actual_weighted_mod, 0},
+    {"_rcpp_module_boot_stan_fit4b_splines_actual_weighted_incidence_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4b_splines_actual_weighted_incidence_mod, 0},
     {"_rcpp_module_boot_stan_fit4linear_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4linear_mod, 0},
     {"_rcpp_module_boot_stan_fit4linear_phe_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4linear_phe_mod, 0},
     {"_rcpp_module_boot_stan_fit4linear_weighted_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4linear_weighted_mod, 0},
