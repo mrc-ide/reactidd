eed94a7e3ad2a30db0b2ad8a6d061c743bbc54e7
Eales96 <odeales@gmail.com>
2023-02-20 17:04:13 +0000

    Finished vignette IFR

--- R/plot_spline_difference.R
+++ R/plot_spline_difference.R
@@ -3,24 +3,13 @@
 #' Bayesian p-spline model using stan
 #'
 #' @export
-#' @param death_dat public data set of death data by age as downloaded from ...
-#' @param min_date minimum date to use
-#' @param max_date maximum date to use
-#' @return A dataframe with columns for all deaths, deaths in those aged 64 and under and for those aged 65 and over
+#' @param X1  dates of first array
+#' @param Y_array1 full posterior of the log of a p-spline with ncol = number of dates
+#' @param X2 dates of second array
+#' @param Y_array2 full posterior of the log of a p-spline with ncol = number of dates
+#' @param time_delay time lag between Y_array1 and Y_array2
+#' @return A dataframe with columns for the log difference between the responses of two different p-spline model fits
 #'
-
-clean_death_data <- function(death_dat, min_date = as.Date("2020-05-01"), max_date=as.Date("2021-08-31")){
-  death_dat <- maditr::dcast(death_dat, date~age, value.var = "deaths")
-  death_dat$age_64_under <- death_dat[,3]+death_dat[,16]
-  death_dat$age_65_over <- death_dat[,17]+death_dat[,18]+death_dat[,19]+death_dat[,20]+death_dat[,21]+death_dat[,22]
-  death_dat$all <- death_dat$age_64_under + death_dat$age_65_over
-  death_dat$date <- as.Date(death_dat$date)
-  death_dat <- death_dat[death_dat$date>= min_date,]
-  death_dat <- death_dat[death_dat$date<= max_date,]
-
-}
-
-
 
 plot_spline_difference <- function(X1, Y_array1,
                                    X2, Y_array2,
@@ -47,4 +36,5 @@ plot_spline_difference <- function(X1, Y_array1,
 
   diff_df
 }
+
 
