ecd444b3d4d7a667f486c58eeb2567ea296d634f
Eales96 <odeales@gmail.com>
2023-02-20 16:24:35 +0000

    updating vignette - need get_response_posterior

--- R/plot_spline_difference.R
+++ R/plot_spline_difference.R
@@ -19,3 +19,32 @@ clean_death_data <- function(death_dat, min_date = as.Date("2020-05-01"), max_da
   death_dat <- death_dat[death_dat$date<= max_date,]
 
 }
+
+
+
+plot_spline_difference <- function(X1, Y_array1,
+                                   X2, Y_array2,
+                                   time_delay){
+  minrow<-min(nrow(Y_array1), nrow(Y_array2))
+  Y_array1 <- Y_array1[1:minrow,]
+  Y_array2 <- Y_array2[1:minrow,]
+
+  X2 <- X2 - time_delay
+  common_dates <- X1[X1 %in% X2]
+
+  diff_df <- data.frame(date = common_dates)
+
+  for(i in seq_len(nrow(diff_df))){
+    temp_date <- diff_df$date[i]
+    index1 <- match(temp_date, X1)
+    index2 <- match(temp_date, X2)
+
+    diff_vec = Y_array1[,index1] - Y_array2[,index2]
+    diff_df$mean[i] <- mean(diff_vec)
+    diff_df$lb[i] <- quantile(diff_vec, c(0.025,0.975))[[1]]
+    diff_df$ub[i] <- quantile(diff_vec, c(0.025,0.975))[[2]]
+  }
+
+  diff_df
+}
+
